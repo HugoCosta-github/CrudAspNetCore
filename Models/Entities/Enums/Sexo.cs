122ce06bec25fa8afbbb069f2fd1b0fba03a58e1
Hugo Costa <hugocosta.ti@gmail.com>
2019-09-22 12:16:11 -0300

    Projeto iniciado. Criada a classe/entidade Jovem

---
+++ Models/Entities/Enums/Sexo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAspNet.Models.Entities.Enums
+{
+    public enum Sexo
+    {
+        Masculino,
+        Feminino
+    }
+}
