1824d9f97835ded2eccf792022401ea215b5601c
hugonc <hugocosta.ti@gmail.com>
2019-09-24 22:06:18 -0300

    Foram criadas as classes/entidades do Projeto

---
+++ src/ApplicationCore/Young.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAspNet.src.ApplicationCore
+{
+    public class Jovem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Nickname { get; set; }
+        public string NameFather { get; set; }
+        public string NomeMother { get; set; }
+        public string Occupation { get; set; }
+        public string ChurchFunction { get; set; }
+        public string LocalityEJC { get; set; }
+        public DateTime DateEJC { get; set; }
+        public string Parish { get; set; }
+        public bool Baptism { get; set; }
+        public bool Eucharist { get; set; }
+        public bool Chrism { get; set; }
+
+
+
+    }
+}
---
+++ src/ApplicationCore/YoungSpy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAspNet.src.ApplicationCore
+{
+    public class YoungSpy
+    {
+    }
+}
